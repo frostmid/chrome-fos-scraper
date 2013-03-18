264fbd6560fdd64c2dc84be548937ee8b9c7ce61
Sergey Antoninko <lyxsus@gmail.com>
2013-03-18 20:53:45 +0400

    dev

--- background/index.js
+++ background/index.js
@@ -1,9 +1,10 @@
-require (['libs/slave', 'features/messages', 'libs/socket.io'], function (Slave, scrapeMessages) {
+require (['libs/slave', 'features/messages', 'features/explain', 'libs/socket.io'], function (Slave, scrapeMessages, scrapeUrl) {
 	// var url = 'http://89.179.119.16:8001';
 	var url = 'http://127.0.0.1:8001';
 
 	(new Slave)
 		.use ('urn:fos:sync:feature/cf6681b2f294c4a7a648ed2bf196df4c', scrapeMessages)
+		.use ('urn:fos:sync:feature/01c4b1eacc107b8de1d977d95a523986', scrapeUrl)
 
 		.fail (function (error) {
 			console.error ('Could not connect to master');

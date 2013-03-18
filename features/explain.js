63de4f1dca9e595a05a6b6f79c3de11ef6c6f004
Sergey Antoninko <lyxsus@gmail.com>
2013-03-18 21:10:06 +0400

    dev

---
+++ features/explain.js
@@ -0,0 +1,33 @@
+define (['libs/scraper', 'libs/q'], function (Scraper, Q) {
+	return function (task) {
+		var emitter = this.emitter (task),
+			scraper = new Scraper (task);
+		
+		return scraper.start ()
+			.then (function (tab) {
+				return scraper.exec ('check-site', tab)
+					.then (function () {
+						return scraper.exec ('is-authorized', tab)
+					})
+
+					.then (function (authorized) {
+						if (!authorized) {
+							return scraper.exec ('authorize', tab);
+						}
+					});
+			})
+
+			.then (function () {
+				return scraper.createTab (task.url);
+			})
+
+			.then (function (tab) {
+				return scraper.exec ('explain', tab)
+					.then (emitter)
+			})
+
+			.fin (function () {
+				return scraper.closeWindow ();
+			});
+	};
+});
